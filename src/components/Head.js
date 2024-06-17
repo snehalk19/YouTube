ef37d6b13e02ff7fbfb5057a8979c68f97d5c568
Snehal Khot <39563593+snehalk19@users.noreply.github.com>
2024-06-17 16:41:56 -0500

    Added search functionality

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { toggleMenu } from "../utils/appSlice";
 import { useDispatch } from "react-redux";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+  };
+
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -26,14 +45,32 @@ const Head = () => {
       </div>
 
       <div className="col-span-10 px-10">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-        />
-        <button className="border border-gray-400 p-2 rounded-r-full py-2 px-5 bg-gray-100 ">
-          🔍
-        </button>
+        <div>
+          <input
+            className=" px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 p-2 rounded-r-full py-2 px-5 bg-gray-100 ">
+            🔍
+          </button>
+        </div>
+        {showSuggestions && (
+          <div className="absolute bg-white py-2 px-2 w-[30rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className=" py-2 px-3 shadow-sm hover:bg-gray-100">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
+
       <div className="col-span-1">
         <img
           className="h-8"
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,14 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
+import { YOUTUBE_VIDEOS_API_BY_ID } from "../utils/constants";
 
 const Watch = () => {
+  const [searchParams] = useSearchParams();
+
+  const [videos, setVideos] = useState([]);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
+    getVideoById();
   }, []);
 
-  return <div>Watch</div>;
+  const getVideoById = async () => {
+    const data = await fetch(
+      YOUTUBE_VIDEOS_API_BY_ID + "&id=" + searchParams.get("v")
+    );
+    const json = await data.json();
+
+    setVideos(json.items[0].snippet.title);
+  };
+
+  return (
+    <div className="px-5">
+      <iframe
+        width="1000"
+        height="500"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+      <div className="font-bold text-xl">{videos}</div>
+    </div>
+  );
 };
 
 export default Watch;
